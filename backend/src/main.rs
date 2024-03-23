886691b135653d469cb3d024b277ae0fe58f9841
Jeremias Ryttäri <ryttari.jeremias@gmail.com>
2024-03-23 11:02:21 +0200

    refactor in a point struct (yes premature i know)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,5 @@
+use std::borrow::Borrow;
+
 use bitflags::bitflags;
 
 bitflags! {
@@ -8,6 +10,19 @@ bitflags! {
         const left = 0b1000;
     }
 }
+#[derive(PartialEq)]
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32
+}
+
+impl Point {
+    pub fn translate(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+}
 #[derive(Debug)]
 struct PlayerInput {
     player_id: i32,
@@ -16,14 +31,12 @@ struct PlayerInput {
 #[derive(Debug)]
 struct Player {
     id: i32,
-    x: i32,
-    y: i32
+    position: Point
 }
 
 impl Player {
     pub fn move_me(&mut self, dx: i32, dy:i32) {
-        self.x += dx;
-        self.y += dy;
+        self.position.translate(dx, dy);
     }
 }
 
@@ -35,28 +48,24 @@ struct GameController {
 
 impl GameController {
     pub fn player_input(&mut self, input: PlayerInput) {
-        let player: Result<&mut Player, String> = self.get_player_by_id(input.player_id);
-        match player {
-            Ok(player) => {
-                let mut dx = 0;
-                let mut dy = 0;
+        let player = self.get_player_by_id(input.player_id).expect("Player not found");
+        let mut dx = 0;
+        let mut dy = 0;
+        let direction = Direction::from_bits(input.input).expect("Invalid input");
                 
-                if input.input & Direction::up.bits() != 0 {
-                    dy -= 1;
-                }
-                if input.input & Direction::down.bits() != 0 {
-                    dy += 1;
-                }
-                if input.input & Direction::left.bits() != 0 {
-                    dx -= 1;
-                }
-                if input.input & Direction::right.bits() != 0 {
-                    dx += 1;
-                }
-                player.move_me(dx, dy);
-            },
-            Err(error) => panic!("{error}")
+        if direction.contains(Direction::up) {
+            dy -= 1;
         }
+        if direction.contains(Direction::down) {
+            dy += 1;
+        }
+        if direction.contains(Direction::left) {
+            dx -= 1;
+        }
+        if direction.contains(Direction::right) {
+            dx += 1;
+        }
+        player.move_me(dx, dy);
     }
     fn get_player_by_id(&mut self, player_id: i32) -> Result<&mut Player, String> {
         if let Some(player) = self.players.iter_mut().find(|player| player.id == player_id) {
@@ -78,14 +87,14 @@ mod tests {
     #[test]
     fn test_player_input() {
         let players: Vec<Player> = vec![
-            Player {id: 1, x: 10, y: 10},
-            Player {id: 2, x: 20, y: 20},
-            Player {id: 3, x: 30, y: 30},
-            Player {id: 4, x: 40, y: 40},
-            Player {id: 5, x: 50, y: 50},
-            Player {id: 6, x: 60, y: 60},
-            Player {id: 7, x: 70, y: 70},
-            Player {id: 8, x: 80, y: 80},
+            Player {id: 1, position: Point{x: 10, y: 10}},
+            Player {id: 2, position: Point{x: 20, y: 20}},
+            Player {id: 3, position: Point{x: 30, y: 30}},
+            Player {id: 4, position: Point{x: 40, y: 40}},
+            Player {id: 5, position: Point{x: 50, y: 50}},
+            Player {id: 6, position: Point{x: 60, y: 60}},
+            Player {id: 7, position: Point{x: 70, y: 70}},
+            Player {id: 8, position: Point{x: 80, y: 80}},
         ];
 
         let mut game_controller = GameController {
@@ -112,14 +121,14 @@ mod tests {
             };
             game_controller.player_input(player_input);
         }
-        assert_eq!(game_controller.players[0].y, 9); // player moved up
-        assert_eq!(game_controller.players[1].x, 21); // player moved right
-        assert_eq!(game_controller.players[2].y, 31); // player moved down
-        assert_eq!(game_controller.players[3].x, 39); // player moved left
-        assert_eq!([game_controller.players[4].x, game_controller.players[4].y], [51, 49]); // player moved up-right
-        assert_eq!([game_controller.players[5].x, game_controller.players[5].y], [59, 59]); // player moved up-left
-        assert_eq!([game_controller.players[6].x, game_controller.players[6].y], [71, 71]); // player moved down-right
-        assert_eq!([game_controller.players[7].x, game_controller.players[7].y], [79, 81]); // player moved down-left
+        assert_eq!(game_controller.players[0].position, Point {x: 10, y: 9}); // player moved up
+        assert_eq!(game_controller.players[1].position, Point {x: 21, y: 20}); // player moved right
+        assert_eq!(game_controller.players[2].position, Point {x: 30, y: 31}); // player moved down
+        assert_eq!(game_controller.players[3].position, Point {x: 39, y: 40}); // player moved left
+        assert_eq!(game_controller.players[4].position, Point {x: 51, y: 49}); // player moved up-right
+        assert_eq!(game_controller.players[5].position, Point {x: 59, y: 59}); // player moved up-left
+        assert_eq!(game_controller.players[6].position, Point {x: 71, y: 71}); // player moved down-right
+        assert_eq!(game_controller.players[7].position, Point {x: 79, y: 81}); // player moved down-left
 
     }
 }
