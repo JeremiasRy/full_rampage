a0623d8bc78686b5acfaeddfb58cbbb941105e70
Jeremias Ryttäri <ryttari.jeremias@gmail.com>
2024-03-29 09:43:36 +0200

    logic for gathering input and only sending when changes occur

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,51 @@
 import GameWindow from "./components/GameWindow";
+import { validInputs } from "./types/input";
 import { isValidInput } from "./utils/helpers";
 import { useEffect, useState } from "react";
+import isEqual from "lodash/isEqual";
 
 function App() {
-  const [keysDown, setKeysDown] = useState<Set<string>>(new Set());
+  const [keysDown, setKeysDown] = useState<Set<number>>(new Set());
+  const [sentInputs, setSentInputs] = useState<number[]>([]);
 
   function handleKeyDown(event: KeyboardEvent) {
-    if (isValidInput(event.code)) {
-      setKeysDown((keys) => new Set([...keys, event.code]));
+    event.preventDefault();
+    if (!isValidInput(event.code)) {
+      return;
     }
+
+    const parsedInput = validInputs[event.code]();
+    setKeysDown((keys) => new Set([...keys, parsedInput]));
   }
 
   function handleKeyUp(event: KeyboardEvent) {
-    if (isValidInput(event.code)) {
-      setKeysDown((keys) => {
-        keys.delete(event.code);
-        return keys;
-      });
+    event.preventDefault();
+    if (!isValidInput(event.code)) {
+      return;
     }
+
+    const parsedInput = validInputs[event.code]();
+    setKeysDown((prevKeys) => {
+      const updatedSet = new Set(prevKeys);
+      updatedSet.delete(parsedInput);
+      return updatedSet;
+    });
   }
 
   // Send users inputs from keysDown to backend for processing
   useEffect(() => {
-    console.log("There is an input!!!");
-    console.log(keysDown);
+    const keysArray = Array.from(keysDown);
+
+    if (isEqual(keysArray, sentInputs)) {
+      return;
+    }
+
+    console.log("Sending input to backend!");
+    console.log(
+      "Input: ",
+      keysArray.reduce((a, b) => a + b, 0)
+    );
+    setSentInputs(keysArray);
   }, [keysDown]);
 
   useEffect(() => {
