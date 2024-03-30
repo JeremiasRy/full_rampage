f63d7f952b667fd4f88cf6f27fca324c9fdbcc7b
Jeremias Ryttäri <ryttari.jeremias@gmail.com>
2024-03-30 09:46:20 +0200

    add a base for game state updater

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,40 @@
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
+use tokio_tungstenite::WebSocketStream;
 use futures_util::{SinkExt, StreamExt};
 use backend::gamelogic::{GameController, PlayerInputRequest};
 
 #[tokio::main]
 async fn main() {
     let game_state = Arc::new(Mutex::new(GameController::new()));
+    let connections = Arc::new(Mutex::new(HashMap::<usize, WebSocketStream<TcpStream>>::new()));
 
     let addr = "127.0.0.1:9999";
     let listener = TcpListener::bind(addr).await.expect("Failed to start server");
 
     println!("Server running at {}!", addr);
 
+    tokio::spawn(connection_handler(listener, game_state.clone(), connections.clone()));
+    tokio::spawn(game_state_updater(game_state.clone(), connections.clone()));
+}
+
+async fn game_state_updater(game_state: Arc<Mutex<GameController>>, connections: Arc<Mutex<HashMap<usize, WebSocketStream<TcpStream>>>>) {
+    loop {
+        let mut controller = game_state.lock().await;
+
+        if controller.should_tick() {
+            controller.tick();
+
+            println!("send new game state to all clients, also limit fps to 60?")
+        }
+    }
+}
+
+async fn connection_handler(listener: TcpListener, game_state: Arc<Mutex<GameController>>, connections: Arc<Mutex<HashMap<usize, WebSocketStream<TcpStream>>>>) {
     loop {
         let (stream, _) = listener.accept().await.expect("Error happened when receiving a request");
 
@@ -46,6 +67,7 @@ async fn handle_connection_inner(stream: TcpStream, game_state: Arc<Mutex<GameCo
                 controller.player_input(player_input);
             }
         } else {
+            println!("New player!!");
             let id_response = controller.add_player();
             write.send(Message::text(id_response.to_string())).await?;
         }
